3f019e19d370eb133667c1b5c3da09f564430589
AnoyingDog <AnoyingDog@users.noreply.github.com>
2026-09-22 17:22:07 +0200

    Check if grs-replay is running.

--- scripts/gpu-screen-recorder.sh
+++ scripts/gpu-screen-recorder.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+if ! systemctl --user is-active --quiet gsr-replay; then
+  notify-send "gsr-replay not running" "Starting the service..."\
+  --urgency critical --app-name "gpu-screen-recorder.sh "
+  systemctl start --user gsr-replay --now
+  exit 1
+fi
 pkill --signal SIGUSR1 -x gpu-screen-reco
 notify-send "Clip saved" "Last 60s written to ~/Videos/Clips"
